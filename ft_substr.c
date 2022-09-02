0dc86579405ef64ba7c9dd3a4e01dd0c7e5c8d3b
Victor <47831936+VictorEgret@users.noreply.github.com>
2022-09-02 18:40:41 +0200

    substr

---
+++ ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/02 17:40:17 by vegret            #+#    #+#             */
+/*   Updated: 2022/09/02 17:40:17 by vegret           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*result;
+	size_t	result_len;
+
+	result_len = ft_strlen(s) - start;
+	if (result_len > len)
+		result_len = len;
+	if (result_len < 0)
+		return (NULL);
+	result = malloc(result_len + 1);
+	if (!result)
+		return (NULL);
+	result = ft_memcpy(result, s + start, result_len);
+	result[result_len] = '\0';
+	return (result);
+}
--- tests.c
+++ tests.c
@@ -9,13 +9,8 @@ int main(int argc, char const *argv[])
     char dest[] = "test";
     int n = 5;
     int *t = ft_calloc(n, sizeof(int));
-
-    for (size_t i = 0; i < n; i++)
-    {
-        printf("FT: %d\n", t[i]);
-    }
     
-    //printf("FT: %d\n", ft_memcmp(dest, src, n));
-    //printf("DE: %d\n", memcmp(dest, src, n));
+    printf("FT: %s\n", ft_substr(src, 0, 2));
+    //printf("DE: %d\n", substr(dest, src, n));
     return 0;
 }
