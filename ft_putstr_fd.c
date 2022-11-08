426538db606efbc143198ed0b8f0c1b76d7a813c
Victor Egret <victor.egret.pro@gmail.com>
2022-11-08 21:30:59 +0100

    Files name fix

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,22 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fd_utils.c                                      :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/05 18:52:05 by vegret            #+#    #+#             */
-/*   Updated: 2022/09/05 18:52:05 by vegret           ###   ########.fr       */
+/*   Created: 2022/11/08 19:43:56 by vegret            #+#    #+#             */
+/*   Updated: 2022/11/08 19:44:12 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include "libft.h"
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
 
 void	ft_putstr_fd(char *s, int fd)
 {
@@ -26,31 +20,5 @@ void	ft_putstr_fd(char *s, int fd)
 	{
 		ft_putchar_fd(*s, fd);
 		s++;
-	}
-}
-
-void	ft_putendl_fd(char *s, int fd)
-{
-	if (!s)
-		return ;
-	ft_putstr_fd(s, fd);
-	ft_putchar_fd('\n', fd);
-}
-
-void	ft_putnbr_fd(int n, int fd)
-{
-	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
-	else if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(-n, fd);
-	}
-	else if (n < 10)
-		ft_putchar_fd(n + '0', fd);
-	else
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
 	}
 }
--- ft_striteri.c
+++ ft_striteri.c
@@ -1,37 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fstr.c                                          :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/05 18:18:28 by vegret            #+#    #+#             */
-/*   Updated: 2022/11/08 13:50:51 by vegret           ###   ########.fr       */
+/*   Created: 2022/11/08 19:40:20 by vegret            #+#    #+#             */
+/*   Updated: 2022/11/08 19:40:29 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdlib.h>
-#include "libft.h"
-
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{
-	char			*result;
-	unsigned int	i;
-
-	if (!s || !f)
-		return (NULL);
-	result = malloc(ft_strlen(s) + 1);
-	if (!result)
-		return (NULL);
-	i = 0;
-	while (s[i])
-	{
-		result[i] = (*f)(i, s[i]);
-		i++;
-	}
-	result[i] = '\0';
-	return (result);
-}
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
